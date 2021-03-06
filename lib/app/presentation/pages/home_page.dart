e4adf383e6229a30e59432aa5e05a5a797949d87
S. M. JAHANGIR <smjxpro@gmail.com>
2021-03-06 23:42:18 +0600

    responsive value

--- lib/app/presentation/pages/home_page.dart
+++ lib/app/presentation/pages/home_page.dart
@@ -2,14 +2,20 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 class HomePage extends GetResponsiveView {
-  build(BuildContext context) {
+  builder() {
     return Scaffold(
       appBar: AppBar(
         title: Text('Home Page'),
       ),
       body: Container(
         child: Center(
-          child: Text('Lorem ipsum dolor sit amet...'),
+          child: Text(
+            'Lorem ipsum dolor sit amet...',
+            style: TextStyle(
+              fontSize:
+                  screen.responsiveValue(mobile: 20, tablet: 30, desktop: 50),
+            ),
+          ),
         ),
       ),
     );
